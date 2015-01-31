aea33fc712b7f43d26a63e70e998fbc9d15ea823
David Z. Jiang <SharkIng@Shacas.com>
2015-01-31 13:16:32 -0500

    Wording function

--- donation.php
+++ donation.php
@@ -1,13 +1,16 @@
 <html>
 	<table style = "width:100%">
 		<?php
-			$product = $dbc -> query ("SELECT * FROM Donation ")
+			Include_once("./libs/global.conf.php");
+			
+			$product = $dbc -> query ("SELECT * FROM Donation ");
+			
 			foreach ($product as $p) { ?>
 			<tr> 
 				<td> <?= $p['Image']?> </td>
-				<td> <?= $p['Name']?> 
+				<td> <?= $p['Name']?> </br>
 					 <?= $p['Description']?> </td>
-				<td> Price: 
+				<td> Price: </br>
 					<?= $p['Price']?> 
 					<Button type="button"> Add </Button>
 					</td>
