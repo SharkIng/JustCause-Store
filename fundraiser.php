7076d4ac5d0ce48258fc2af616eb01892fd63c7b
wangxi1214 <wangxi12140924@gmail.com>
2015-01-31 13:17:58 -0500

    fundraiser php

--- fundraiser.php
+++ fundraiser.php
@@ -1,1 +1,21 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+<html>
+	<table style = "width:100%">
+		<?php
+			Include_once("./libs/global.conf.php");
+			
+			$product = $dbc -> query ("SELECT * FROM Fundrasier ");
+			
+			foreach ($product as $p) { ?>
+			<tr> 
+				<td> <?= $p['Image']?> </td>
+				<td> <?= $p['Name']?> </br>
+					 <?= $p['Description']?> </td>
+				<td> Price: </br>
+					<?= $p['Price']?> 
+					<Button type="button"> Add </Button>
+					</td>
+			</tr>
+			<?php
+				} 
+			?>
+</html>
