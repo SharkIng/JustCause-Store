bdd2f43d10e5f09882d3810142aed5a015a3db23
akasaki <akasaki@AkasakitekiMacBook-Air.local>
2015-01-31 18:13:07 -0500

    format donation

--- donation.php
+++ donation.php
@@ -1,18 +1,117 @@
+<style type="text/css">
+.link {
+	text-align: center;
+	-moz-box-shadow:inset 0px 34px 0px -15px #b54b3a;
+	-webkit-box-shadow:inset 0px 34px 0px -15px #b54b3a;
+	box-shadow:inset 0px 34px 0px -15px #b54b3a;
+	background-color:#a73f2d;
+	border:1px solid #ffffff;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:arial;
+	font-size:28px;
+	font-weight:bold;
+	padding:10px 52px;
+	text-decoration:none;
+	text-shadow:0px -1px 0px #7a2a1d;
+}
+.link:hover {
+	background-color:#b34332;
+}
+.link:active {
+	position: relative;
+	top: 1px;
+	text-align: center;
+}
+.p{
+	max-height:30%;
+	}
+.name{
+	font-weight: bold;
+	color: #900;
+	font-size: x-large;
+	}
+.des{
+	color: #900;
+	font-size: medium;
+	}
+.priceN{
+	front-size:350%;
+	text-align:center
+	}
+.image{
+	max-height:30%		
+	}
+
+
+
+.AddButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #fff6af;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #fff6af;
+	box-shadow:inset 0px 1px 0px 0px #fff6af;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffec64), color-stop(1, #ffab23));
+	background:-moz-linear-gradient(top, #ffec64 5%, #ffab23 100%);
+	background:-webkit-linear-gradient(top, #ffec64 5%, #ffab23 100%);
+	background:-o-linear-gradient(top, #ffec64 5%, #ffab23 100%);
+	background:-ms-linear-gradient(top, #ffec64 5%, #ffab23 100%);
+	background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffec64', endColorstr='#ffab23',GradientType=0);
+	background-color:#ffec64;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #ffaa22;
+	display:inline-block;
+	cursor:pointer;
+	color:#d60808;
+	font-family:arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:8px 13px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffee66;
+}
+.AddButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffab23), color-stop(1, #ffec64));
+	background:-moz-linear-gradient(top, #ffab23 5%, #ffec64 100%);
+	background:-webkit-linear-gradient(top, #ffab23 5%, #ffec64 100%);
+	background:-o-linear-gradient(top, #ffab23 5%, #ffec64 100%);
+	background:-ms-linear-gradient(top, #ffab23 5%, #ffec64 100%);
+	background:linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffab23', endColorstr='#ffec64',GradientType=0);
+	background-color:#ffab23;
+}
+.AddButton:active {
+	position:relative;
+	top:1px;
+}
+
+.price {
+	font-size: 24px;
+	color: #350417;
+}
+
+</style>
 <html>
-	<table style = "width:100%">
+        <p> <input type="button" onClick="window.open('donation.php')" value="Donation" class="link" align="center">
+        <input type="button" onClick="demoDisplay2()" value="Fundraiser" class="link" align="center"></p>
+        
+	<table width="90%" align="center" border="1" bgcolor="#CCCCCC" bordercolor="#FFFFFF" background="#CCCCCC">
 		<?php
 			Include_once("./libs/global.conf.php");
 			
 			$product = $dbc -> query ("SELECT * FROM Donation WHERE category = 'donation'");
 			
 			foreach ($product as $p) { ?>
-			<tr> 
-				<td> <?= $p['Image']?> </td>
-				<td> <?= $p['Name']?> </br>
-					 <?= $p['Description']?> </td>
-				<td> Price: </br>
-					<?= $p['Price']?> 
-					<Button type="button"> Add </Button>
+			<tr class="p"> 
+				<td class="image" width="20%"> <?= $p['Image']?> </td>
+				<td width="40%"> 
+                	<tr class="name"><?= $p['Name']?> </tr>
+					<tr class="des"><?= $p['Description']?> </tr></td>
+				<td width="30%"> <tr class="price">Price: </tr>
+					<tr class="priceN"><?= $p['Price']?> </tr>
+					<tr><Button type="button" class="AddButton"> Add to Cart</Button></tr>
 					</td>
 			</tr>
 			<?php
