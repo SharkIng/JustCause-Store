ec713caf9caef3d06e71b10c9e65a4ec527a9469
wangxi1214 <wangxi12140924@gmail.com>
2015-01-31 13:06:47 -0500

    donation php

--- donation.php
+++ donation.php
@@ -1,1 +1,18 @@
-skdjaflhajpe
+<html>
+	<table style = "width:100%">
+		<?php
+			$product = $dbc -> query ("SELECT * FROM Donation ")
+			foreach ($product as $p) { ?>
+			<tr> 
+				<td> <?= $p['Image']?> </td>
+				<td> <?= $p['Name']?> </ br>
+					 <?= $p['Description']?> </td>
+				<td> Price: </ br>
+					<?= $p['Price']?> </ br>
+					<Button type="button"> Add </Button>
+					</td>
+			</tr>
+			<?php
+				} 
+			?>
+</html>
