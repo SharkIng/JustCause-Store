bb93e823a81bd1429715935ea2d85db8e047fdca
wangxi1214 <wangxi12140924@gmail.com>
2015-01-31 11:53:31 -0500

    Back end files

--- description.php
+++ description.php
@@ -1,6 +1,19 @@
 <html>
 	<div class = "description">
-		<
+		<div class = "Donation">
+			donation
+		</div>
+		<div class = "Fundraiser">
+			fundraiser
+		</div>
+	</div>
+	<div class = "display_php">
+		<frameset>
+			<frame src="donation.php" name = "view_php">
+			store display
+		</frameset>
+	</div>
+		
 
 
 </html>
