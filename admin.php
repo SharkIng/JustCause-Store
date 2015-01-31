79872f40b05cc1b90a7fb425078c2e5e6fd00f36
frankfuyujiang <frankfuyujiang@gmail.com>
2015-01-31 13:53:53 -0500

    Update admin.php

---
+++ admin.php
@@ -0,0 +1,20 @@
+
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
