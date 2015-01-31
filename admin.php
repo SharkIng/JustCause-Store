76a6816ddf940c526d21000254c83b166c7e7231
wangxi1214 <wangxi12140924@gmail.com>
2015-01-31 15:25:49 -0500

    admin

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
+<?php
 
+Include_once("./libs/global.conf.php");
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
@@ -18,3 +20,27 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error updating record: " . $conn->error;
 }
+*/
+$new_pic = $_POST["add_pic"];
+$new_name = $_POST["add_name"];
+$new_description = $_POST["add_descrip"];
+$new_price = $_POST["add_price"];
+$new_stocks = $_POST["add_stocks"];
+if (isset($_POST["add"])){
+	if(isset($_POST["donation"])){
+		$dbc->query("INSERT INTO Donation("$new_name", "new_pic", "$new_description", "new_price","new_stocks")");
+	}
+	else {
+		$dbc->query("INSERT INTO Donation("$new_name", "new_pic", "$new_description", "new_price","new_stocks")");
+	}
+}
+//need javascript to check if there is a product selected
+$current_product = $_POST["current"];
+$current_category = $_POST["category"];
+if (isset($_POST["del"])){
+	$dbc ->query("DELETE FROM $current_category WHERE Name=$current_product");
+}
+
+
+
+?>
