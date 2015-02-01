63c0e469d8a525be39a33473f39ea8d5f1f6e6d5
wangxi1214 <wangxi12140924@gmail.com>
2015-01-31 19:21:58 -0500

    cart

--- donation.php
+++ donation.php
@@ -101,7 +101,12 @@
 	border-style: solid;
 	}
 </style>
+
+<?php
+session_start();
+?>
 <html>
+
         <p> <input type="button" onClick="window.open('donation.php')" value="Donation" class="link" align="center">
         <input type="button" onClick="window.open('fundraiser.php')" value="Fundraiser" class="link" align="center"></p>
         
@@ -109,25 +114,55 @@
 		<?php
 			Include_once("./libs/global.conf.php");
 			
-			$product = $dbc -> query ("SELECT * FROM Donation WHERE category = 'donation'");
-			
+			$product = $dbc -> query ("SELECT * FROM Donation WHERE category = 'donation'");?>
+			<form action="cartUpdate.php" enctype="multipart/form-data" method="post">
+			<?php
 			foreach ($product as $p) { ?>
 			<tr class="p"> 
 				<td class="image" width="20%"> <?= $p['Image']?> </td>
 				<td width="40%"> 
                 <table border="0">
-               	  <tr class="name"><?= $p['Name']?> </tr>
+               	  <tr class="name"><input type="radio" name="productsID" value="<?= $p['ID']?>"/>
+				  <label for ="productsID"> <?= $p['Name']?></label> </tr>
 					<tr class="des"><?= $p['Description']?> </tr></table></td>
 				<td width="30%"> 
                 <table border="0">
                 <tr class="price">Price: </tr>
 					<tr class="priceN"><?= $p['Price']?> </tr>
-					<tr><Button type="button" class="AddButton"> Add to Cart</Button></tr>
+					<tr><input type="submit" value = "Add to Cart" class="AddButton"/></tr>
                   </table>
 			  </td>
 			</tr>
 			<?php
 				} 
 			?>
+			
+<div class="shopping-cart">
+<h2>Your Shopping Cart</h2>
+<?php
+if(isset($_SESSION["cart"]))
+{
+    $total = 0;
+    echo '<ol>';
+    foreach ($_SESSION["cart"] as $items)
+    {
+        echo '<li class="cart-itm">';
+        echo '<span class="remove-itm"><a href="cartUpdate.php?productsID='.$items["id"].'&return_url='.$current_url.'">&times;</a></span>';
+        echo '<h3>'.$items["name"].'</h3>';
+        echo '<div class="p-code">P code : '.$items["id"].'</div>';
+        echo '<div class="p-qty">Qty : '.$items["quantity"].'</div>';
+        echo '<div class="p-price">Price : $'.$items["price"].'</div>';
+        echo '</li>';
+        $subtotal = ($items["price"]*$items["quantity"]);
+        $total = ($total + $subtotal);
+    }
+    echo '</ol>';
+    echo '<span class="check-out-txt"><strong>Total : $'.$total.'</strong> <a href="view_cart.php">Check-out!</a></span>';
+	echo '<span class="empty-cart"><a href="cartUpdate.php?empty=1&return='.$current_url.'">Empty Cart</a></span>';
+}else{
+    echo 'Your Cart is empty';
+}
+?>
+</div>
         </table>
 </html>
