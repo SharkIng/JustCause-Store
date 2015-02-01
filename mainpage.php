6aa12237ee3bca0570811506a10112b0bcc5d08a
akasaki <akasaki@AkasakitekiMacBook-Air.local>
2015-01-31 19:20:16 -0500

    main page with login button

--- mainpage.php
+++ mainpage.php
@@ -6,6 +6,76 @@
 </head>
 <style type="text/css">
 
+.login {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #f6f6f6));
+	background:-moz-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-webkit-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-o-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-ms-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#f6f6f6',GradientType=0);
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
+}
+.login:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f6f6f6), color-stop(1, #ffffff));
+	background:-moz-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+	background:-webkit-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+	background:-o-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+	background:-ms-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);
+	background:linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f6f6f6', endColorstr='#ffffff',GradientType=0);
+	background-color:#f6f6f6;
+}
+.login:active {
+	position: relative;
+	top: 1px;
+	text-align: right;
+}
+
+
+    .black_overlay{
+        display: none;
+        position: absolute;
+        top: 0%;
+        left: 0%;
+        width: 100%;
+        height: 100%;
+        background-color: black;
+        z-index:1001;
+        -moz-opacity: 0.8;
+        opacity:.80;
+        filter: alpha(opacity=80);
+    }
+    .white_content {
+        display: none;
+        position: absolute;
+        top: 25%;
+        left: 25%;
+        width: 50%;
+        height: 50%;
+        padding: 16px;
+        border: 16px solid orange;
+        background-color: white;
+        z-index:1002;
+        overflow: auto;
+    }
+
 .link {
 	text-align: center;
 	-moz-box-shadow:inset 0px 34px 0px -15px #b54b3a;
@@ -65,7 +135,7 @@
     <td width="30%"><img src="../image/description.png" width="389" height="486"></td>
     <td width="60%">
       <p>
-        <input type="button" onclick="window.open('donation.php')" value="Donation" class="link" align="center">
+        <input type="button" onClick="window.open('donation.php')" value="Donation" class="link" align="center">
         <input type="button" onClick="demoDisplay2()" value="Fundraiser" class="link" align="center">
       </p>
 <p>
@@ -78,9 +148,21 @@
       </p>
       <p class="context"><span class="context">Please scroll down for details on our current giving options and click "add to cart" to select one. </p>
       <p class="context"><span class="context">Or, simply click the Donate button below to enter your own amount and request where it goes.</p>
-      <p class="context">Thank you!</p></td>
+      <p class="context">Thank you!</p>
+          <p align="right"><a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'" class="login">Login</a></p>
+    <div id="light" class="white_content">
+    <form action="verify.php" method="post">
+    User Name:<br>
+    <input type="text" name="username"><br><br>
+    Password:<br>
+    <input type="password" name="password"><br><br>
+    <input type="submit" name="submit" value="Login">
+</form><a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+    <div id="fade" class="black_overlay"></div></td>
   </tr>
 </table>
+
+
 <!--
     </div>
     <div class = "buttons">
